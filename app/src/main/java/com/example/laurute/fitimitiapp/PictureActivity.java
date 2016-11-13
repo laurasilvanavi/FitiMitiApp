d6e1ac363a1b16816e9aaed499ab5c363b6c52e2
Laurute <laraa3@gmail.com>
2016-11-13 11:51:16 +0200

    Išsaugo nuotrauką nurodytu pavadinimu "FitiMitiApp_MetaiDienaLaikas.jpeg" ir nurodytoje vietoje "FitiMitiApp"", kurią gali pasiekti vartotojas.

--- app/src/main/java/com/example/laurute/fitimitiapp/PictureActivity.java
+++ app/src/main/java/com/example/laurute/fitimitiapp/PictureActivity.java
@@ -29,90 +29,52 @@ public class PictureActivity extends AppCompatActivity {
     }
 
     public void takeaPicture(View view) {
-        dispatchTakePictureIntent();
+        String state;
+        state = Environment.getExternalStorageState();
+
+        if(Environment.MEDIA_MOUNTED.equals(state)) { //kitoks tikrinimas ar suteiktos teises
+            dispatchTakePictureIntent();
+        }
+         else{
+              Toast.makeText(getApplicationContext(), "Jeigu norite įvykdyti funkciją, turi leisti prieiti prie jūsų duomenų.", Toast.LENGTH_LONG).show();
+         }
     }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Toast.makeText(getApplicationContext(), "Atsiprašome, bet įvyko klaida kuriant failą. Suteikite teises programėlei pasiekti ir naudoti Jūsų duomenis.", Toast.LENGTH_LONG).show();
+
+            }
+            // sukurtas buvo failas, o dbr fotkinama
+            if (photoFile != null) {
+                Uri photoURI=Uri.fromFile(new File(mCurrentPhotoPath));
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
-//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        // Ensure that there's a camera activity to handle the intent
-//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-//            // Create the File where the photo should go
-//            File photoFile = null;
-//            try {
-//                Toast.makeText(getApplicationContext(), "first", Toast.LENGTH_LONG).show();
-//                photoFile = createImageFile();
-//                Toast.makeText(getApplicationContext(), "good", Toast.LENGTH_LONG).show();
-//            } catch (IOException ex) {
-//                // Error occurred while creating the File
-//                Toast.makeText(getApplicationContext(), "Negerai, kol kūrė failą", Toast.LENGTH_LONG).show();
-//
-//            }
-//            // Continue only if the File was successfully created
-//            if (photoFile != null) {
-//                Uri photoURI = FileProvider.getUriForFile(this,
-//                        "com.example.android.fileprovider",
-//                        photoFile);
-//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-//                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-//                Toast.makeText(getApplicationContext(), "good2", Toast.LENGTH_LONG).show();
-//            }
-//        }
    }
 
-
     private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
 
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "FitiMitiApp_" + timeStamp + "_"; //Jpeg_
+        File storageDir = Environment.getExternalStoragePublicDirectory("FitiMitiApp");
+        if(!storageDir.exists()){ //jei nera tokio aplanko, tai sukuriam
+            storageDir.mkdir();
+            Toast.makeText(getApplicationContext(), "Nebuvo, tad sukuriam", Toast.LENGTH_LONG).show();
+         }
+
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        mCurrentPhotoPath = image.getAbsolutePath();
         return image;
     }
-//    public void writeExternalStorage(View view){
-//        //tikrinam
-//        String state;
-//        state = Environment.getExternalStorageState();
-//
-//        if(Environment.MEDIA_MOUNTED.equals(state)){ //kitoks tikrinimas ar suteiktos teises
-//            //File root = new File(this.getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "manoAplankas");// gaunam direktorija(nuotrauku aplankale), kuri bus prieinama tik mūsų appsui
-//            File root = Environment.getExternalStoragePublicDirectory("manoAplankas"); // gaunam direktorija, kuri bus prieinama visiems
-//            if(!root.exists()){ //ar dar neegzistuoja folderis
-//                root.mkdir(); //kuriam
-//                Toast.makeText(getApplicationContext(), "Nebuvo, tad sukuriam", Toast.LENGTH_LONG).show();
-//            }
-//            File file = new File(root, "myMessage.txt");
-//            String message = ed1.getText().toString();
-//            try {
-//                FileOutputStream fileOutputStream= new FileOutputStream(file);
-//                fileOutputStream.write(message.getBytes());
-//                fileOutputStream.close();
-//                ed1.setText("");
-//                textView.setText("Jeigu norite, įveskite dar kartą:");
-//                Toast.makeText(getApplicationContext(), "zinute issaugota, liux", Toast.LENGTH_LONG).show();
-//            } catch (FileNotFoundException e) {
-//                e.printStackTrace();
-//                Toast.makeText(getApplicationContext(), "Nerado failiuko", Toast.LENGTH_LONG).show();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                Toast.makeText(getApplicationContext(), "IoException", Toast.LENGTH_LONG).show();
-//            }
-//
-//        }
-//        else{
-//            Toast.makeText(getApplicationContext(), "Neleidzia naudori saugyklos", Toast.LENGTH_LONG).show();
-//        }
-//
-//    }
 
 }
