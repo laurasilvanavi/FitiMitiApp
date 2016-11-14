3b0d2cf7ae3e83af4cf6f468957df295c7b1c526
Laurute <laraa3@gmail.com>
2016-11-14 14:59:37 +0200

    Random funkcija su vardais.

--- app/src/main/java/com/example/laurute/fitimitiapp/Fragments/PlayerFragment.java
+++ app/src/main/java/com/example/laurute/fitimitiapp/Fragments/PlayerFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.laurute.fitimitiapp.R;
 
@@ -13,9 +14,14 @@ import com.example.laurute.fitimitiapp.R;
  */
 
 public class PlayerFragment extends Fragment {
+    TextView textViewPlayer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_player, container, false);
+        View view = inflater.inflate(R.layout.fragment_player, container, false);
+        textViewPlayer = (TextView)view.findViewById(R.id.textViewPlayer);
+        String player = getArguments().getString("Player");
+        textViewPlayer.setText(player);
+        return view;
     }
 }
--- app/src/main/java/com/example/laurute/fitimitiapp/GameActivity.java
+++ app/src/main/java/com/example/laurute/fitimitiapp/GameActivity.java
@@ -4,10 +4,14 @@ import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.laurute.fitimitiapp.Database.GameDbHelper;
 import com.example.laurute.fitimitiapp.Fragments.PlayerFragment;
 import com.example.laurute.fitimitiapp.Fragments.SimpleTaskFragment;
 import com.example.laurute.fitimitiapp.Fragments.WhatFragment;
 import com.example.laurute.fitimitiapp.Fragments.WhoFragment;
+import com.example.laurute.fitimitiapp.Object.Task;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity implements WhatFragment.WhatListener, WhoFragment.WhoListener {
 
@@ -32,6 +36,7 @@ public class GameActivity extends AppCompatActivity implements WhatFragment.What
 
 
     }
+    GameDbHelper db;
 
     @Override
     public void ShowWhat() {
@@ -51,7 +56,24 @@ public class GameActivity extends AppCompatActivity implements WhatFragment.What
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         PlayerFragment playerFragment = new PlayerFragment();
 
+        db = new GameDbHelper(this);
+        int random = getRandom(db.getAllPlayers().size(), 0);
+        String player = db.getAllPlayers().get(random).toString(); // cia veliau turi buti ne uzduoties
+//        String task_description =task.get_description();
+
+        Bundle info = new Bundle();
+        info.putString("Player", player);
+        playerFragment.setArguments(info);
+
+
         fragmentTransaction.replace(R.id.fragment_container1, playerFragment);
         fragmentTransaction.commit();
+    }
+
+    private int getRandom(int max, int min)
+    {
+        Random r = new Random();
+        int result = r.nextInt(max - min + 1) + min;
+        return result;
     }
 }
