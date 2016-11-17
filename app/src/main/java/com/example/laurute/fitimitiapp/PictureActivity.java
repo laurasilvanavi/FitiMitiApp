a5f4161b188f502c7114da56b11f27d731717ea5
Laurute <laraa3@gmail.com>
2016-11-18 00:13:22 +0200

    Įgyvendintas veidų atpažinimo nuotraukos užduotyje. Jeigu nėra pakankamo skaičiau veidų padarytoje nuotraukoje- neleidžiama grįžti į žaidimą. Pridėta dependencies bibliotekos ir manifeste prie GameActivity nurodyta, jog nesaugoma istorija (negalima eiti atgal ir matyti, kas buvo).

--- app/src/main/java/com/example/laurute/fitimitiapp/PictureActivity.java
+++ app/src/main/java/com/example/laurute/fitimitiapp/PictureActivity.java
@@ -1,31 +1,62 @@
 package com.example.laurute.fitimitiapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class PictureActivity extends AppCompatActivity {
-    static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_TAKE_PHOTO = 1;
     String mCurrentPhotoPath;
+    Uri photoURI;
+    ImageView imageView;
+    TextView textView;
+    int peopleCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
+        imageView =(ImageView) findViewById(R.id.imageViewPhoto);
+        textView=(TextView) findViewById(R.id.textViewRepeatPhotoTask);
+        //Intent intent = getIntent();
+        //String message = intent.getStringExtra(GameActivity.EXTRA_MESSAGE);
+        String message = "1";
+        try
+        {
+            int number = Integer.parseInt(message);
+            peopleCount = number;
+        }
+        catch (NumberFormatException e)
+        {
+            peopleCount = 1;
+        }
+        dispatchTakePictureIntent();
     }
 
     public void takeaPicture(View view) {
@@ -36,6 +67,7 @@ public class PictureActivity extends AppCompatActivity {
             dispatchTakePictureIntent();
         }
          else{
+
               Toast.makeText(getApplicationContext(), "Jeigu norite įvykdyti funkciją, turi leisti prieiti prie jūsų duomenų.", Toast.LENGTH_LONG).show();
          }
     }
@@ -54,12 +86,31 @@ public class PictureActivity extends AppCompatActivity {
             }
             // sukurtas buvo failas, o dbr fotkinama
             if (photoFile != null) {
-                Uri photoURI=Uri.fromFile(new File(mCurrentPhotoPath));
+                photoURI=Uri.fromFile(new File(mCurrentPhotoPath));
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
    }
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO) {
+            if (resultCode == RESULT_OK) {
+                int facesCount = detectFaces();
+                if (facesCount>=peopleCount){
+                    Intent intent = new Intent(PictureActivity.this, GameActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    textView.setText("Net pirmokas suskaičiuotų, jog per mažai žmonių nuotraukoje!");
+                }
+            }
+            else{
+                textView.setText("Kur bėgate? Dar nenusifotografavote!");
+            }
+        }
+    }
 
     private File createImageFile() throws IOException {
 
@@ -74,5 +125,54 @@ public class PictureActivity extends AppCompatActivity {
 
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
+    }
+
+    private int detectFaces(){
+        Bitmap picture = rotatePicture();
+        FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
+                .setTrackingEnabled(false)
+                .build();
+
+        Frame frame = new Frame.Builder().setBitmap(picture).build();
+        SparseArray<Face> faces = detector.detect(frame); //dedami rasti veidai
+
+        int count = faces.size();
+        Toast.makeText(getApplicationContext(), "Aptikta veidų: "+count, Toast.LENGTH_LONG).show();
+        detector.release();
+        imageView.setImageBitmap(picture);
+        return count;
+    }
+
+    private Bitmap rotatePicture(){
+        BitmapFactory.Options bounds = new BitmapFactory.Options(); //nustatomas ilgis ir plotis
+        bounds.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bounds);
+
+
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        Bitmap bm = BitmapFactory.decodeFile(mCurrentPhotoPath, opts);
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(mCurrentPhotoPath);        //info apie foto padeti
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "Sugadinta nuotrauka", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
+        int orientation = orientString != null ? Integer.parseInt(orientString) :  ExifInterface.ORIENTATION_NORMAL;
+
+        int rotationAngle = 0;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
+
+        Matrix matrix = new Matrix();
+        matrix.setRotate(rotationAngle, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
+        return rotatedBitmap;
+    }
+    @Override
+    public void onBackPressed() {
+        //moveTaskToBack(true);
     }
 }
